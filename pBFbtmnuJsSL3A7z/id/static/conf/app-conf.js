0af542c2b57a20bae586534b6c2099868de6e5c0
vmlankub <45290401+vmlankub@users.noreply.github.com>
2020-03-06 19:37:10 +0800

    Original path: D:/orz/www.cmlink.com/id/static/conf/app-conf.js

---
+++ pBFbtmnuJsSL3A7z/id/static/conf/app-conf.js
@@ -0,0 +1,12 @@
+window.APP_CONF = {
+  /*
+  基础URL，同axios配置中的baseURL。如果配置
+  了URL，那么本配置将覆盖axios.config.js中
+  的baseURL配置，如果配置了空字符串或null，
+  那么这里的配置将被忽略
+  */
+  baseURL: {
+    cloud: '',
+    osp: '',
+  },
+}
